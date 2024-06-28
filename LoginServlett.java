062089a33f050053b6e415aabe5114c8fd5ac1fd
Tarun Gurram <gurramtarun29@gmail.com>
2024-06-28 22:07:29 +0530

    Update LoginServlett.java

--- LoginServlett.java
+++ LoginServlett.java
@@ -1,16 +1,5 @@
 package com.fauxy.backend;
 
-import jakarta.servlet.ServletException;
-
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-
-import java.io.IOException;
-import com.fauxy.dao.*;
-
 @WebServlet("/login")
 public class LoginServlett extends HttpServlet {
 	private static final long serialVersionUID = 1L;
